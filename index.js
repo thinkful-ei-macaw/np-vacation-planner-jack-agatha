bef6ab3011fac07dd1d554b1c111a9c91638d214
Jack Lansing <jack.lansing@live.com>
2020-02-20 17:01:29 -0500

    completed functions and added minor styling

--- index.js
+++ index.js
@@ -13,22 +13,21 @@ const clickSubmit = function() {
 };
 
 const formatQueryParameters = function(params) {
-  //TODO: Craft query params
   const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 };
 
 const getNationalParks = function(query, maxResult=10) {
-  //TODO: Fetch national parks from API
   const params = {
     api_key: apiKey,
     stateCode: query,
     limit: maxResult,
+    fields: 'addresses',
   };
+
   const queryString = formatQueryParameters(params);
   const url = searchURL + '?' + queryString;
-  console.log(url);
 
   fetch(url)
     .then(response => {
@@ -40,17 +39,25 @@ const getNationalParks = function(query, maxResult=10) {
     .then(responseJson => displayResults(responseJson))
     .catch(err => {
       $('#js-error-message').text(`Mistakes have been made: ${err.message}`);
-    })
+    });
 };
 
 const displayResults = function(responseJson) {
-  //TODO: Generate results HTML and add to page
-  console.log(responseJson);
-  $('#js-search-results').empty();
-  for (let i = 0; i < responseJson.items.length; i++){
-    $('js-search-results').append(
-        `<li><h2>${responseJson.items[i].}`
-    )
+  const { data } = responseJson;
+  $('.js-search-results').empty();
+  for (let i = 1; i <= data.length; i++){
+    let { name, description, url, addresses } = data[i];
+    let { postalCode, city, stateCode, line1 } = addresses[1];
+    $('.js-search-results').append(
+      `
+    <li>
+    <h2>${name}</h2>
+    <address>${line1}<br>${city},${stateCode} ${postalCode}</address>
+    <a href="${url}">Click here for more info</a>
+    <p>${description}</p>
+    </li>
+   `
+    );
   }
 };
 
